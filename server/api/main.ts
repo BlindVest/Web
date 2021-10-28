855bbdaa2a92cefb31c7deb062b21d4a17bcffee
chul0721 <chul070721@naver.com>
2021-10-28 20:18:21 +0900

    feat: shock

--- server/api/main.ts
+++ server/api/main.ts
@@ -4,13 +4,14 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 const main = async (req: Request, res: Response) => {
-  const { distance, latitude, longitude } = req.body
+  const { distance, latitude, longitude, shock } = req.body
 
   const result = await prisma.data.create({
     data: {
       distance: distance,
       latitude: latitude,
-      longitude: longitude
+      longitude: longitude,
+      shock: shock
     }
   })
 
